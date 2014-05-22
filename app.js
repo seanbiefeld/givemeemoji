e7654edb3e66adc872260c625785fe485e9ca862
Sean Biefeld <seanbiefeld@Biefeld-MacBook-Pro.local>
2014-05-22 13:53:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+fs = require('fs');
+http = require('http');
+url = require('url');
+
+
+http.createServer(function(req, res){
+  var request = url.parse(req.url, true);
+  var imageName = request.pathname;
+
+  try {
+    console.log('about to load ./images/emojis'+imageName+'.png');
+     var img = fs.readFileSync('./images/emojis'+imageName+'.png');
+     res.writeHead(200, {'Content-Type': 'image/png' });
+     res.end(img, 'binary');
+  }
+  catch(exception) { 
+    console.log(exception);
+     res.writeHead(200, {'Content-Type': 'text/plain' });
+     res.end('Exists not, image you requested. \n');
+  }
+}).listen(process.env.PORT || 3000);
